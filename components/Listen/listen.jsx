707dd09ff9e8dbf70056b4056f78e8bf990f0bff
seunsanyaa <seunsanyaa@gmail.com>
2022-01-31 21:56:25 +0200

    change animationn

--- components/Listen/listen.jsx
+++ components/Listen/listen.jsx
@@ -30,25 +30,29 @@ export default function Listen( ) {
 
     useEffect(() => {
 
-gsap.from([gistContainer.current],
-    {scrollTrigger:{
-        trigger:gistContainer2.current,
-            scrub:true,
-            toggleActions: "restart pause resume none"
-            // pin: true
-        },
-        x:"100%",
-        duration:3,
-        // ease: CustomEase.create("custom", "M0,0 C0.11,0.494 0.192,0.726 0.318,0.852 0.45,0.984 0.504,1 1,1")
+                gsap.from([gistContainer.current], {
+                        x:"100%",
+                        duration:3,
+                        scrollTrigger: [gistContainer.current]
 
 
-        },
+                        // scrollTrigger:{
+                        //     trigger:gistContainer2.current,
+                        //         scrub:true,
+                        //         toggleActions: "restart pause resume none"
+                        //         // pin: true
+                        //     },
+
+                        // ease: CustomEase.create("custom", "M0,0 C0.11,0.494 0.192,0.726 0.318,0.852 0.45,0.984 0.504,1 1,1")
+
+
+                        },
 
 
 
 
 
-    )
+                    )
 
 
 
