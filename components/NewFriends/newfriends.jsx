666ab612cd10f598fbb3a8078833a459c8cc5e94
seunsanyaa <seunsanyaa@gmail.com>
2022-02-02 13:09:01 +0200

    circle

--- components/NewFriends/newfriends.jsx
+++ components/NewFriends/newfriends.jsx
@@ -55,8 +55,47 @@ export default function NewFriends( ) {
 <img className={newfriendsstyles.sweater} src="https://res.cloudinary.com/seunsanyaa/image/upload/v1643756490/Ellipse_180_k36b5j.png"/>
           </div>
 
-          <img className={newfriendsstyles.circleMobile} src="https://res.cloudinary.com/seunsanyaa/image/upload/v1643063219/Group_1623_2_idcsdp.png"/>
+          {/*<img className={newfriendsstyles.circleMobile} src="https://res.cloudinary.com/seunsanyaa/image/upload/v1643063219/Group_1623_2_idcsdp.png"/>*/}
 
+
+          <div className={newfriendsstyles.circleMobile}>
+
+
+
+                  <img className={newfriendsstyles.line1Mobile} src="https://res.cloudinary.com/seunsanyaa/image/upload/v1643792875/Ellipse_164_3_m4znnk.png"/>
+
+
+
+
+
+
+                  <img className={newfriendsstyles.line2Mobile}  src="https://res.cloudinary.com/seunsanyaa/image/upload/v1643794470/Ellipse_163_2_nvt8z6.png"/>
+
+<img className={newfriendsstyles.line3Mobile} src="https://res.cloudinary.com/seunsanyaa/image/upload/v1643795682/Ellipse_162_4_corr1o.png"/>
+<img className={newfriendsstyles.line4Mobile} src="https://res.cloudinary.com/seunsanyaa/image/upload/v1643797405/Ellipse_161_3_y54o11.png"/>
+
+              <div className={newfriendsstyles.newfriendsbgMobile}>
+
+                  <div>
+
+
+
+                      <h3 className={newfriendsstyles.header}>
+                          Make new friends
+                      </h3>
+
+                      <p className={newfriendsstyles.paragraph}>Searching for a potential match...</p>
+                  </div>
+
+              </div>
+          <img className={newfriendsstyles.afroguyImageMobile} src="https://res.cloudinary.com/seunsanyaa/image/upload/v1643795743/Ellipse_179_1_vgy2e8.png"/>
+          <img className={newfriendsstyles.sweaterMobile} src="https://res.cloudinary.com/seunsanyaa/image/upload/v1643795813/Ellipse_180_2_dcobni.png"/>
+          <img className={newfriendsstyles.coverFaceMobile} src="https://res.cloudinary.com/seunsanyaa/image/upload/v1643796298/Ellipse_181_1_wj74mg.png"/>
+          <img className={newfriendsstyles.southAfricaMobile} src="https://res.cloudinary.com/seunsanyaa/image/upload/v1643796729/Ellipse_182_1_ein8su.png"/>
+          <img className={newfriendsstyles.afroladyImageMobile} src="https://res.cloudinary.com/seunsanyaa/image/upload/v1643797054/Ellipse_178_1_gashas.png"/>
+          <img className={newfriendsstyles.blueImageMobile} src="https://res.cloudinary.com/seunsanyaa/image/upload/v1643797982/Ellipse_177_3_bpokxf.png"/>
+
+          </div>
 
       </div>
 
